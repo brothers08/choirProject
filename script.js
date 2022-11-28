3a5c3e6ef5b4a30e86ab7d4e6bb3c100237b2d14
Ciara B <45543828+brothers08@users.noreply.github.com>
2022-11-27 18:39:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function openModal(curImg) {
+    const body = document.querySelector("body");
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById(curImg);
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    //var captionText = img.alt;
+
+    body.style.overflow = "hidden";
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    body.style.overflow = "auto";
+    }
+
+};
+
+
+
